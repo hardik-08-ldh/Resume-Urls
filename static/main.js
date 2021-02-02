8cf3db695c2774c312229725b4eb2ad996b409ea
Hardik <punjhardik2000@gmail.com>
2021-02-02 10:22:23 +0530

    Ready to deploy

--- static/main.js
+++ static/main.js
@@ -7,7 +7,7 @@ let previous=null
 copyBtn.forEach(btn=> btn.addEventListener("click", ()=> {
     const url=btn.getAttribute('data-url')
     navigator.clipboard.writeText(url)
-    btn.textContent='URL Copied!!!'
+    btn.textContent='URL Copied To Clipboard'
 
     if (previous){
         previous.textContent="click to copy URL"
